903f59d391cea203fdbb111fbd71c60023a7a9de
qu-bit1 <103852686+qu-bit1@users.noreply.github.com>
2023-09-24 06:04:52 +0530

    Add dummy pages

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,21 +1,98 @@
 import styled from 'styled-components';
+import AutofitGrid from 'components/AutofitGrid';
+import BasicCard from 'components/BasicCard';
 import Page from 'components/Page';
-import FaqSection from 'views/PricingPage/FaqSection';
-import PricingTablesSection from 'views/PricingPage/PricingTablesSection';
+import SectionTitle from 'components/SectionTitle';
+import YoutubeVideo from 'components/YoutubeVideo';
+import { media } from 'utils/media';
 
-export default function PricingPage() {
+const FEATURES = [
+  {
+    imageUrl: '/grid-icons/asset-1.svg',
+    title: 'Lorem ipsum dolor sit amet.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error dolorem ipsa dolore facere est consequuntur aut, eos doloribus voluptate?',
+  },
+  {
+    imageUrl: '/grid-icons/asset-2.svg',
+    title: 'Lorem ipsum dolor sit amet.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error dolorem ipsa dolore facere est consequuntur aut, eos doloribus voluptate?',
+  },
+  {
+    imageUrl: '/grid-icons/asset-3.svg',
+    title: 'Lorem ipsum dolor sit amet.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error dolorem ipsa dolore facere est consequuntur aut, eos doloribus voluptate?',
+  },
+  {
+    imageUrl: '/grid-icons/asset-4.svg',
+    title: 'Lorem ipsum dolor sit amet.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error dolorem ipsa dolore facere est consequuntur aut, eos doloribus voluptate?',
+  },
+  {
+    imageUrl: '/grid-icons/asset-5.svg',
+    title: 'Lorem ipsum dolor sit amet.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error dolorem ipsa dolore facere est consequuntur aut, eos doloribus voluptate?',
+  },
+  {
+    imageUrl: '/grid-icons/asset-6.svg',
+    title: 'Lorem ipsum dolor sit amet.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error dolorem ipsa dolore facere est consequuntur aut, eos doloribus voluptate?',
+  },
+  {
+    imageUrl: '/grid-icons/asset-7.svg',
+    title: 'Lorem ipsum dolor sit amet.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error dolorem ipsa dolore facere est consequuntur aut, eos doloribus voluptate?',
+  },
+  {
+    imageUrl: '/grid-icons/asset-8.svg',
+    title: 'Lorem ipsum dolor sit amet.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error dolorem ipsa dolore facere est consequuntur aut, eos doloribus voluptate?',
+  },
+  {
+    imageUrl: '/grid-icons/asset-9.svg',
+    title: 'Lorem ipsum dolor sit amet.',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis error dolorem ipsa dolore facere est consequuntur aut, eos doloribus voluptate?',
+  },
+];
+
+export default function FeaturesPage() {
   return (
-    <Page title="Pricing" description="Cupidatat et reprehenderit ullamco aute ullamco anim tempor.">
+    <Page title="Features" description="Elit aute do nisi Lorem id ea culpa sint duis eu tempor dolore elit.">
       <Wrapper>
-        <PricingTablesSection />
-        <FaqSection />
+        <SectionTitle>Check out this quick introduction</SectionTitle>
+        <YoutubeVideo url="https://www.youtube.com/watch?v=BggrpKfqh1c" />
+        <CustomAutofitGrid>
+          {FEATURES.map((singleFeature, idx) => (
+            <BasicCard key={singleFeature.title} {...singleFeature} />
+          ))}
+        </CustomAutofitGrid>
       </Wrapper>
     </Page>
   );
 }
 
 const Wrapper = styled.div`
-  & > :last-child {
-    margin-bottom: 15rem;
+  & > *:not(:first-child) {
+    margin-top: 10rem;
+  }
+`;
+
+const CustomAutofitGrid = styled(AutofitGrid)`
+  --autofit-grid-item-size: 40rem;
+
+  ${media('<=tablet')} {
+    --autofit-grid-item-size: 30rem;
+  }
+
+  ${media('<=phone')} {
+    --autofit-grid-item-size: 100%;
   }
 `;
